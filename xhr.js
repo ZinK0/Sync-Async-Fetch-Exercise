3c39ebbe2f63acadeb2f50a150efb8c319238a66
Zin KO <zinko.it@outlook.com>
2024-08-08 00:04:16 +0630

    Separate the code for xhr and callback example

--- xhr.js
+++ xhr.js
@@ -1,12 +1,13 @@
 // what is XMLHttpRequest ?
 // API link = https://api.openweathermap.org/geo/1.0/direct?q=Yangon&limit=5&appid=7243ae234ab17718e54f80392370a3e9
+
 let geolocation_api =
   "https://api.openweathermap.org/geo/1.0/direct?q=Yangon&limit=5&appid=7243ae234ab17718e54f80392370a3e9";
 
 let weather_api =
   "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
 
-function browser(callback, api) {
+function browser(api) {
   let browser = new XMLHttpRequest();
 
   // We got our response but have to figure out to get pure data
@@ -15,16 +16,9 @@ function browser(callback, api) {
   browser.addEventListener("readystatechange", () => {
     if ((browser.readyState === 4) & (browser.status === 200)) {
       let data = JSON.parse(browser.responseText);
-      //   console.log(data);
-      //   console.log(typeof data);
-      //   console.log(data[0].name);
-
-      //   let lat = data[0].lat;
-      //   let lon = data[0].lon;
-      callback(data);
-
-      //   console.log(data[0].lat);
-      //   console.log(data[0].lon);
+      console.log(data);
+      console.log(typeof data);
+      console.log(data[0].name);
     }
   });
 
@@ -32,20 +26,4 @@ function browser(callback, api) {
   browser.send();
 }
 
-function getGeolocation(data) {
-  console.log(data);
-  let lat = data[0].lat;
-  let lon = data[0].lon;
-
-  console.log("Latitude => ", lat);
-  console.log("Longitude => ", lon);
-  let weather_api_modi = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7243ae234ab17718e54f80392370a3e9`;
-  browser(showWeather, weather_api_modi);
-}
-
-function showWeather(data) {
-  console.log(data);
-  console.log("Yangon Weather is =>", data.weather[0].description);
-}
-
-browser(getGeolocation, geolocation_api);
+browser(geolocation_api);
