4f3eba09ce9ecf2163e613312ffc87009a8e0d24
Zin KO <zinko.it@outlook.com>
2024-08-09 15:14:06 +0630

    Added fetch current weather data

--- weather app/app.js
+++ weather app/app.js
@@ -1,4 +1,5 @@
 // Take user action like input the city
+let api_key = "7243ae234ab17718e54f80392370a3e9";
 let geolocation_api =
   "https://api.openweathermap.org/geo/1.0/direct?q=yangon&limit=5&appid=7243ae234ab17718e54f80392370a3e9";
 
@@ -15,14 +16,22 @@ let geolocation_api =
 // console.log(searchCity);
 // Testing End ...
 
-async function getWeatherData() {
+async function getWeatherData(api_key) {
   try {
     let getGeolocation = await fetch(geolocation_api);
     let geoData = await getGeolocation.json();
     let latitude = geoData[0].lat;
     let longitude = geoData[0].lon;
+    let currentWeather_api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${api_key}`;
 
-    let currentWeather = await fetch();
+    let currentWeather = await fetch(currentWeather_api);
+    let currentWeatherData = await currentWeather.json();
+    console.log(currentWeatherData);
+    console.log(
+      `Current ${currentWeatherData.name} => `,
+      currentWeatherData.weather[0].description
+    );
+    console.log("Current Temperature =>", currentWeatherData.main.temp, "°C");
   } catch (error) {}
 }
-getWeatherData();
+getWeatherData(api_key);
